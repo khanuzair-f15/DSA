781031abe8a5677d1560b0fc7d9e547201c04d11
uzair khan <khanuzair.work@gmail.com>
2026-09-04 20:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (41.70%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        // the code below uses unsorted set means this code has significantly
+        // high space requierment
+        /*
         unordered_set<int> s;
 
         for (int i = 0; i < nums.size(); i++) {
@@ -10,5 +13,21 @@ public:
             s.insert(nums[i]);
         }
         return 0;
+        ☻*/
+        // now in order to reduce space complexity also we will use floyds cycle
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // find entrance of floyds cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
