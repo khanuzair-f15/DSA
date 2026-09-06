9480e6fae8a9e60ac0f94aef846d11d35fb74ca3
uzair khan <khanuzair.work@gmail.com>
2026-09-07 00:55:56 +0530

    Time: 16 ms (8.16%) | Memory: 23.7 MB (12.82%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (s.count(nums[i])) {
+                s.erase(nums[i]);
+            } else {
+                s.insert(nums[i]);
+            }
+        }
+        int x = *s.begin();
+        return x;
+    }
+};
