0cb961f4095df0f41744c61430b24de51f4875fb
uzair khan <khanuzair.work@gmail.com>
2026-09-08 19:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (68.79%) - LeetSync

--- 2383-add-two-integers/add-two-integers.cpp
+++ 2383-add-two-integers/add-two-integers.cpp
@@ -1,7 +1,4 @@
 class Solution {
 public:
-    int sum(int num1, int num2) {
-        long x = num1 + num2;
-        return (int)x;
-    }
+    int sum(int num1, int num2) { return num1 + num2; }
 };
