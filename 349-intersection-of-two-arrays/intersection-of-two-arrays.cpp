9d23e35cf4236efc3aebdc983a30d89a4cb38842
uzair khan <khanuzair.work@gmail.com>
2026-09-13 21:12:13 +0530

    Time: 3 ms (38.27%) | Memory: 14.3 MB (56.84%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            s.insert(nums1[i]);
+        }
+
+        for (int i = 0; i < nums2.size(); i++) {
+            if (s.count(nums2[i])) {
+                ans.push_back(nums2[i]);
+                s.erase(nums2[i]);
+            }
+        }
+
+        return ans;
+        // using stl set intersection
+        /*
         set<int> result; // used to store the common element
         // sort the vector so i can do the set intersection
         sort(nums1.begin(), nums1.end());
@@ -12,5 +29,6 @@ public:
 
         vector<int> ans(result.begin(), result.end());
         return ans;
+        */
     }
 };
