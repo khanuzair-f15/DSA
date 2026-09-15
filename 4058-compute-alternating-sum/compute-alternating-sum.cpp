4e0e6056139f728f0769f485a73573ae7f112abd
uzair khan <khanuzair.work@gmail.com>
2026-09-16 01:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (79.05%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.cpp
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -3,7 +3,7 @@ public:
     int alternatingSum(vector<int>& nums) {
 
         int sum1 = 0;
-        int sum2 = 0;
+ 
 
         for (int i = 0; i < nums.size(); i++) {
             if (i % 2 == 0) {
