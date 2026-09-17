0f99cd69abadbcd8d22cb5e27c523e75f651690d
uzair khan <khanuzair.work@gmail.com>
2026-09-17 17:13:05 +0530

    Time: 4 ms (23.35%) | Memory: 26.6 MB (6.22%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+//Beats 49.05%
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+
         unordered_set<int> s;
         vector<int> v;
         for (auto i : nums) {
