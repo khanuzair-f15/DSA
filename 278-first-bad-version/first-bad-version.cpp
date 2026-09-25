612060ef3d6dff28ec4b3d7d471901014d69bdf8
uzair khan <khanuzair.work@gmail.com>
2026-09-25 20:51:18 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (41.12%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,15 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (isBadVersion(i)) {
+                cout << isBadVersion(i);
+                return i;
+            }
+        }
+        return 0;
+    }
+};
