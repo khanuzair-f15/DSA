3b35bf390783c5db6db44501aa3ece5c1a676075
uzair khan <khanuzair.work@gmail.com>
2026-09-25 21:21:29 +0530

    Time: 3 ms (17.12%) | Memory: 8 MB (5.31%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,14 +4,29 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-
+        /*
         for (int i = 0; i <= n; i++) {
             if (isBadVersion(i)) {
                 cout << isBadVersion(i);
                 return i;
             }
         }
-        
-        return 0;
+        */
+
+        if (isBadVersion(1))
+            return 1;
+        else {
+            int low = 2, high = n, mid;
+            while (low <= high) {
+                mid = low + (high - low) / 2;
+                if (isBadVersion(mid) && isBadVersion(mid - 1) == false)
+                    return mid;
+                else if (isBadVersion(mid) && isBadVersion(mid - 1) == true)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            return mid;
+        }
     }
 };
