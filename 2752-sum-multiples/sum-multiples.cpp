43f620c64ba95b390b92ee6134a24a66a4d7065c
uzair khan <khanuzair.work@gmail.com>
2026-09-26 23:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.56%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        unordered_set<int> s;
+
         long long sum = 0;
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 || i % 7 == 0 || i % 5 == 0) {
